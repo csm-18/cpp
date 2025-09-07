e345aed464f6d1c8e19c53fcd1130a16651ae972
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-09-07 21:24:38 +0530

    update cpp-config.lua boilerplate

--- test/cpp-config.lua
+++ test/cpp-config.lua
@@ -3,6 +3,8 @@ return {
     compiler = "g++",
     compiler_options = "-Wall -Wextra",
     cpp_standard = "c++17",
+    source_files = {"src/*.cpp","src/**/*.cpp"}
+    include_dirs = {"src",},
     output = "hello"
 
 }    
