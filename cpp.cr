833b90c5996274c8f6e0fdd09d51fcc5c3928ca4
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-09-08 22:42:06 +0530

    cli: add about,version commands

--- cpp.cr
+++ cpp.cr
@@ -1,1 +1,14 @@
-puts "Hello!"
+
+VERSION = "0.1.0"
+def main()
+    if ARGV.size == 0
+        puts "cpp #{VERSION}"
+        puts "A build tool and pkg manager for C++"
+    elsif ARGV.size == 1
+        if ARGV[0] == "--version" || ARGV[0] == "-v"
+            puts "cpp #{VERSION}"
+        end    
+    end       
+end
+
+main()
