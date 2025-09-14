259d4a919c2b386dd8e29dea8bb6aee386e1b6ef
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-09-14 23:47:38 +0530

    add get_string_args() function
    to convert c string cli args into c++ string vector

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
-int main()
+
+vector<string> get_string_args(int argc,char* argv[]){
+    vector<string> args;
+
+    for(int i = 0; i < argc; ++i){
+        args.push_back(string(argv[i]));
+    }
+    return args;
+}
+
+int main(int argc,char* argv[])
 {
-    cout << "Hello world!" << endl;
+    vector<string> args = get_string_args(argc,argv);
+    cout << args.size() << endl;
+    for(string arg:args){
+        cout << arg<<endl;
+    }
     return 0;
 }
