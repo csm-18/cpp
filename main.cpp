9ed46090e24bbf2763260f04a1d711d838a50cc4
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-09-15 22:57:29 +0530

    cli: add about,version commands

--- main.cpp
+++ main.cpp
@@ -12,12 +12,19 @@ vector<string> get_string_args(int argc,char* argv[]){
     return args;
 }
 
+
+const string VERSION = "0.1.0";
 int main(int argc,char* argv[])
 {
     vector<string> args = get_string_args(argc,argv);
-    cout << args.size() << endl;
-    for(string arg:args){
-        cout << arg<<endl;
+
+    if(args.size() == 1){
+        cout << "cpp " << VERSION << endl;
+        cout << "A build tool and pkg manager for C++" << endl;
+    }else if(args.size() == 2){
+        if(args[1] == "--version" || args[1] == "-v" || args[1] == "version" || args[1] == "-version"){
+            cout << "cpp " << VERSION << endl;
+        }
     }
     return 0;
 }
